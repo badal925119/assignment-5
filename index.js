c5a99f883d592c7d7be358759e8c8cad4c9ef018
Badal <badalhasanbd5@gmail.com>
2024-09-23 19:00:07 +0600

    added function

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+
+function showAlart(id){
+    const alartBox = document.getElementById('message')
+    alartBox.classList.remove('hidden')
+
+    document.getElementById('alert-message').innerText= id
+    setTimeout(function() {
+        alartBox.classList.add('hidden');
+    }, 2000); 
+
+}
+
 // global section
 const heroSection = document.getElementById('hero-section')
+const footerEl = document.getElementById('footer')
+let mainBlance = parseFloat(document.getElementById('main-blance').innerText);
+const donateHistory = document.getElementById('donate-history')
 
 
+
+
+// blog click section
+document.getElementById('blog').addEventListener('click', function(){
+    window.location.href ='./QA.html'
+})
 
 // Donate and History button section
 const donateTab =document.getElementById('donate-tab')
@@ -10,6 +31,8 @@ historyTab.addEventListener('click', function(){
     historyTab.classList.add('bg-primary','text-black',)
     donateTab.classList.remove('bg-primary', 'text-black')
     heroSection.classList.add('hidden')
+    footerEl.classList.add('hidden')
+    donateHistory.classList.remove('hidden')
 
 })
 
@@ -17,9 +40,38 @@ donateTab.addEventListener('click', function(){
     donateTab.classList.add('bg-primary','text-black')
     historyTab.classList.remove('bg-primary', 'text-black')
     heroSection.classList.remove('hidden')
+    footerEl.classList.remove('hidden')
+    donateHistory.classList.add('hidden')
 })
 
-// blog click section
-document.getElementById('blog').addEventListener('click', function(){
-    window.location.href ='./QA.html'
+// donate Button-1 function
+document.getElementById('donate-button1').addEventListener('click', function(){
+   const donateTk = parseFloat(document.getElementById('donate-tk').innerText);
+   const donateInput = parseFloat(document.getElementById('donate-input-tk').value);
+   if(donateInput <= 0){
+    showAlart('Minumum Amount 1tk')
+    return;
+   } 
+
+   if(donateInput > mainBlance){
+    showAlart('Not Enough Blance')
+    return;
+   }
+   const newMain = mainBlance-donateInput;
+   const totalDonate = donateTk+donateInput;
+   mainBlance=newMain
+   document.getElementById('main-blance').innerText=newMain;
+   document.getElementById('donate-tk').innerText=totalDonate;
+   showAlart('Dontion sucsessfull')
+   
+   const div = document.createElement('div')
+   div.innerHTML=`
+   <div class ="bg-gray-200 my-3 p-3 rounded-md border-l-2 border-indigo-500">
+   <p class="font-lexend font-bold text-2xl">${donateInput} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+  <p class="font-lexend">${new Date}</p>
+    
+    </div>`
+    donateHistory.insertBefore(div,donateHistory.firstChild)
+ 
+    
 })
